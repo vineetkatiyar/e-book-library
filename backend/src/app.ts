f2bfbdc24f7b9e00534a34ec6b7c282070bcc211
Vineet Katiyar <vineetkatiyar98@gmail.com>
2025-11-01 16:14:24 +0530

    cors setup in the backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,16 @@ import express, { Request, Response, NextFunction, ErrorRequestHandler } from 'e
 import { errorHandler } from './middleware/globalErrorHndler';
 import userRouter from './user/user.routes';
 import bookRouter from './books/book.router';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Hello World!' });
