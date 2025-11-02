0ab82198f57c91be0e649198a08f98295a6a3568
Vineet Katiyar <vineetkatiyar98@gmail.com>
2025-11-02 12:10:46 +0530

    creating new frontend client project and installing shadcn ui

---
+++ cleint/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Source_Serif_4 as Source_Serif_Pro } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const serif = Source_Serif_Pro({
+  subsets: ["latin"],
+  variable: "--font-serif",
+});
+
+export const metadata: Metadata = {
+  title: "e-book platform",
+  description: "e-book platform built with MERN stack and Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={`${geist.variable} ${serif.variable}`}>
+      <body className="font-sans antialiased">
+        {children}
+        <Toaster position="top-right" />
+      </body>
+    </html>
+  );
+}
